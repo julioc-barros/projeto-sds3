378c00c44c800b0c1cc02a0c401e9df120c2fa36
julioc-barros <juliocbarros339@gmail.com>
2021-10-22 12:04:01 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/julioc-barros" target="_blank" rel="noreferrer">Júlio Barros</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/julioc.barross" target="_blank" rel="noreferrer">@julioc.barross</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
